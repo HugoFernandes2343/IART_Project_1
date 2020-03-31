61502f34f14dc8a1c51748cb80c543db9a5ae2fb
Ricardofpinto <ricardofpinto98@gmail.com>
2020-03-31 16:25:08 +0100

    recursivar os metodos de scoring e de contagem de tags

--- src/Scoring.java
+++ src/Scoring.java
@@ -1,69 +1,80 @@
 import java.util.ArrayList;
 
-public class Scoring
-{
-    public Scoring(){}
+public class Scoring {
+    public Scoring() {
+    }
 
-    public static int calculatePoints(Slide a, Slide b)
-    {
+    public static int calculatePoints(Slide a, Slide b) {
         ArrayList<String> tagsA = a.getTags();
 
         ArrayList<String> tagsB = b.getTags();
 
-        int common = getCommonTags(tagsA, tagsB);
+        ArrayList<Integer> tags = getNTags(tagsA, tagsB);
+        ArrayList<Integer> tags1 = getNTags(tagsA, tagsB);
 
-        int uncommon1 = getUncommonTags(tagsA, tagsB);
+        int common = tags.get(0);
+        int uncommon1 = tags.get(1);
+        int uncommon2 = tags1.get(2);
 
-        int uncommon2 = getUncommonTags(tagsB, tagsA);
-
-        int score = getMinimumValue(common,uncommon1,uncommon2);
+        int score = getMinimumValue(common, uncommon1, uncommon2);
 
         return score;
     }
 
     private static int getMinimumValue(int common, int uncommon1, int uncommon2) {
-        if(common <= uncommon1 && common <= uncommon2)
+        if (common <= uncommon1 && common <= uncommon2)
             return common;
-        else if(uncommon1 <= common && uncommon1 <= uncommon2)
+        else if (uncommon1 <= common && uncommon1 <= uncommon2)
             return uncommon1;
-        else if(uncommon2 <= uncommon1 && uncommon2 <= common)
+        else if (uncommon2 <= uncommon1 && uncommon2 <= common)
             return uncommon2;
         return 0;
     }
 
-    private static int getCommonTags(ArrayList<String> a, ArrayList<String> b)
-    {
+    public static ArrayList<Integer> getNTags(final ArrayList<String> a, final ArrayList<String> b) {
+        ArrayList<String> a1 = (ArrayList) a.clone();
         int common = 0;
-
-        for (String tag : a) {
-            if(b.contains(tag))
-                common++;
-        }
-
-        return common;
-    }
-
-    private static int getUncommonTags(ArrayList<String> a, ArrayList<String> b)
-    {
         int uncommon = 0;
-
-        for (String tag : a) {
-            if(!b.contains(tag))
+        if (a1.size() > 1) {
+            if (b.contains(a1.get(0))) {
+                common++;
+            } else {
                 uncommon++;
+            }
+            a1.remove(0);
+            ArrayList<Integer> aux = getNTags(a1, b);
+            ArrayList<Integer> ret = new ArrayList<Integer>();
+            ret.add(aux.get(0) + common);
+            ret.add(aux.get(1) + uncommon);
+            return ret;
+        } else {
+            if (b.contains(a1.get(0))) {
+                common++;
+            } else {
+                uncommon++;
+            }
+            ArrayList<Integer> ret = new ArrayList<Integer>();
+            ret.add(common);
+            ret.add(uncommon);
+            return ret;
         }
-
-        return uncommon;
     }
 
-    public static int getTotalScoring(ArrayList<Slide> slideshow)
-    {
+    public static int getTotalScoring(final ArrayList<Slide> slideshow) {
+        final int temp = 0;
+        return getTotalScoringRecursive(slideshow, temp);
+    }
+
+    public static int getTotalScoringRecursive(final ArrayList<Slide> slideshow, final int aux) {
+        ArrayList<Slide> slideshow2 = (ArrayList) slideshow.clone();
         int temp = 0;
-
-        for (int i = 0; i < slideshow.size() - 1; i++) {
-            temp = temp + calculatePoints(slideshow.get(i), slideshow.get(i+1));
+        temp = aux + calculatePoints(slideshow2.get(0), slideshow2.get(1));
+        if (slideshow2.size() > 2) {
+            slideshow2.remove(0);
+            return getTotalScoringRecursive(slideshow2, temp);
+        } else {
+            return temp;
         }
-
-        return temp;
     }
 
 }
